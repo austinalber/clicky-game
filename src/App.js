54d2eed0a1963b1229eb939a418b0757b624fab5
Austin Alber <austinalber@gmail.com>
2019-10-22 15:11:52 -0400

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "./components/NavBar";
+// import Characters from "./characters.json"
+// import Card from "./components/Card"
+// import ScoreBar from "./components/ScoreBar"
+
+function App() {
+  return (
+    <NavBar />
+  );
+}
+
+export default App;
