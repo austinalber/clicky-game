ffd8f5292b4dd0406de937a2ea040ac12323cfeb
Austin Alber <austinalber@gmail.com>
2019-10-22 18:28:11 -0400

    Changed to react-hook layout

--- src/App.js
+++ src/App.js
@@ -1,69 +1,65 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import { useAlert } from "react-alert";
 import NavBar from "./components/NavBar";
 import characters from "./characters.json"
 import Card from "./components/Card"
 import "./app.css"
 
-class App extends Component {
-  state = {
-    characters,
-    score: 0,
-    highScore: 0
-  }
+const App = () => {
+  const alert = useAlert();
+  const charactersList = characters;
+  const [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
 
-  gameOver = () => {
-    if (this.state.score > this.state.highScore) {
-      this.setState({highScore: this.state.score}, function() {
-        console.log(this.state.highScore);
-      });
+  const gameOver = () => {
+    if (score > highScore) {
+      setHighScore(score);
     }
-    this.state.characters.forEach(character => {
+    charactersList.forEach(character => {
       character.count = 0;
     });
-    alert(`Game Over :( \nscore: ${this.state.score}`);
-    this.setState({score: 0});
+    console.log(`Game Over! Your Score: ${score}`);
+    alert.error(`Game Over! Your Score: ${score}`);
+    setHighScore(score);
+    setScore(0);
     return true;
   }
 
-  clickCount = id => {
-    this.state.characters.find((event, i) => {
+  const clickCount = id => {
+    charactersList.find((event, i) => {
       if (event.id === id) {
-        if(characters[i].count === 0){
-          characters[i].count = characters[i].count + 1;
-          this.setState({score : this.state.score + 1}, function(){
-            console.log(this.state.score);
-          });
-          this.state.characters.sort(() => Math.random() - 0.5)
+        if(charactersList[i].count === 0){
+          charactersList[i].count = charactersList[i].count + 1;
+          setScore(score + 1)
+          charactersList.sort(() => Math.random() - 0.5)
           return true; 
         } else {
-          this.gameOver();
+          gameOver();
         }
       }
     });
   }
 
-  render() {
-    return(
-      <div 
-        className="wrapper"
-        style = {{
-          backgroundImage: 'url(' + "http://getwallpapers.com/wallpaper/full/1/4/7/29914.jpg" + ')',
-          backgroundSize: 'cover',
-          // overflow: 'hidden',
-        }}
-      >
-        <NavBar score={this.state.score} highScore={this.state.highScore}/>
-        {this.state.characters.map(characters => (
-          <Card
-            clickCount={this.clickCount}
-            id={characters.id}
-            key={characters.id}
-            image={characters.image}
-          />
-        ))}
-      </div>
-    );
-  }
+  return(
+    <div 
+      className="wrapper"
+      style = {{
+        backgroundImage: 'url("http://getwallpapers.com/wallpaper/full/1/4/7/29914.jpg")',
+        backgroundSize: "cover",
+        overflow: "hidden"
+      }}
+    >
+      <NavBar score={score} highScore={highScore}/>
+      {charactersList.map(charactersList => (
+        <Card
+          clickCount={clickCount}
+          id={charactersList.id}
+          key={charactersList.id}
+          image={charactersList.image}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default App;
